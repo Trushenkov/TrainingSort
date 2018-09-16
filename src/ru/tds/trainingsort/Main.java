dc208f49dc11334904d7eddcf3ad06dd5416bd84
Trushenkov Dmitry <diman28051999@gmail.com>
2018-09-16 21:31:35 +0300

    Main - created.

---
+++ src/ru/tds/trainingsort/Main.java
@@ -0,0 +1,142 @@
+package ru.tds.trainingsort;
+
+import java.io.*;
+import java.util.*;
+
+/**
+ * Класс, в котором реализовано сортировка массива по частоте повторений числа по убыванию.
+ * Если 2 элемента имеют одинаковую частоту, выводится тот, который был первым.
+ * <p>
+ * Date: 06.09.2018 (четверг)
+ * Project name: TrainingSort
+ * Package name: ru.tds.trainingsort
+ *
+ * @author Трушенков Дмитрий 15ИТ18
+ */
+public class Main {
+
+    private static final String INPUT_FILE = "input.txt";
+    private static final String OUTPUT_FILE = "output.txt";
+    private static final String REGEX_FOR_SPLIT = " ";
+
+    public static void main(String[] args) {
+        ArrayList<Integer> arrayList = readFromFile();
+        System.out.println("Исходный массив: " + arrayList);
+        sortNumbersByFrequency(arrayList);
+        System.out.println("Отсортированный массив: " + arrayList);
+        writeArrayToFile(arrayList);
+    }
+
+    /**
+     * Метод для чтения содержимого файла fileName.
+     *
+     * @return ArrayList с числами, прочитанными из файла
+     */
+    private static ArrayList<Integer> readFromFile() {
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(INPUT_FILE))) {
+            String string;
+            String[] splitString;
+            while ((string = reader.readLine()) != null) {
+                splitString = string.split(REGEX_FOR_SPLIT);
+                for (String aSplitString : splitString) {
+                    arrayList.add(Integer.parseInt(aSplitString));
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return arrayList;
+    }
+
+
+    /**
+     * Сортирует ArrayList of Integer по частоте появления в нём чисел в порядке убывания.
+     *
+     * @param array ArrayList of Integer, который необходимо отсортировать
+     */
+    private static void sortNumbersByFrequency(ArrayList<Integer> array) {
+        ArrayList<NumberWithFrequency> numbersContainer = new ArrayList<>();
+        for (int num : array) {
+            boolean foundedRepeat = false;
+            for (NumberWithFrequency currentNumber : numbersContainer) {
+                if (num == currentNumber.value) {
+                    foundedRepeat = true;
+                    currentNumber.frequency++;
+                    break;
+                }
+            }
+            if (!foundedRepeat) {
+                numbersContainer.add(new NumberWithFrequency(num));
+            }
+        }
+        sortIntRateContainerListByRate(numbersContainer);
+        array.clear();
+        for (NumberWithFrequency currentNum : numbersContainer) {
+            while (currentNum.frequency-- > 0) {
+                array.add(currentNum.value);
+            }
+        }
+    }
+
+    /**
+     * Сортирует ArrayList с объектами NumberWithFrequency по полю frequency в порядке убывания.
+     *
+     * @param arrayList ArrayList с объектами NumberWithFrequency
+     */
+    private static void sortIntRateContainerListByRate(ArrayList<NumberWithFrequency> arrayList) {
+        NumberWithFrequency tmp;
+        int start = 0;
+        int end = arrayList.size() - 1;
+        do {
+            for (int i = 0; i < end; i++) {
+                if (arrayList.get(i).frequency < arrayList.get(i + 1).frequency) {
+                    tmp = arrayList.get(i);
+                    arrayList.set(i, arrayList.get(i + 1));
+                    arrayList.set(i + 1, tmp);
+                }
+            }
+            end--;
+            for (int i = end; i > start; i--) {
+                if (arrayList.get(i).frequency > arrayList.get(i - 1).frequency) {
+                    tmp = arrayList.get(i);
+                    arrayList.set(i, arrayList.get(i - 1));
+                    arrayList.set(i - 1, tmp);
+                }
+            }
+            start++;
+        } while (start < end);
+    }
+
+
+    /**
+     * Метод для записи массива в текстовый файл output.txt
+     *
+     * @param arrayList список, который нужно записать в текстовый файл
+     */
+    private static void writeArrayToFile(ArrayList<Integer> arrayList) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(OUTPUT_FILE))) {
+            for (int element : arrayList) {
+                writer.write(String.valueOf(element) + " ");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Класс объекта NumberWithFrequency, в котором хранится число и кол-во его повторений в списке.
+     */
+    private static class NumberWithFrequency {
+
+        int value; // само число
+
+        int frequency; // кол-во его повторений в списке
+
+        NumberWithFrequency(int value) {
+            this.frequency = 1;
+            this.value = value;
+        }
+    }
+
+}
